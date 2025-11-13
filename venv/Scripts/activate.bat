86b3765466a181453f0642ad4d466dafa4f14b38
Leandro <leandrowork1@gmail.com>
2025-11-13 20:07:05 -0300

    cambio en el header

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Estudiante\Downloads\d\Diario-el-centinela-Mercedes-Backend\venv
+set VIRTUAL_ENV=C:\Users\Estudiante\Downloads\d\Agencia-Atenas-Backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
